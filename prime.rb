0fcbc467849f3b76991bf3922c41b726596b0d52
Elle Faraday <faraday.elle@gmail.com>
2017-10-30 18:14:20 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,13 +3,27 @@ def prime?(num)
   if num.negative? || num == 1 || num == 0
     false
   else
-    checks = []
+    checks = [2]
     range = (2..num).to_a
     range.each {|x|
-      if checks.all? {|check| x % check != 0}
-        checks << x
+      prime = true
+      i = 0
+      while prime && i < checks.length
+        if x % checks[i] == 0 && x != checks[i]
+          prime = false
+        else
+          i += 1
+        end
+        if prime
+          checks << x
+        end
       end
+      #if checks.any? {|check| x % check == 0}
+      #else
+        #checks << x
+      #end
     }
+    binding.pry
     if checks.last == num
       true
     else
